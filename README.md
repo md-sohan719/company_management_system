2ae12a60c7ad5882d3fc2f296ffab3b3804e1e69
sohan <sohanmd381@gmail.com>
2023-03-01 11:57:40 +0600

    dashboard

--- README.md
+++ README.md
@@ -7,9 +7,9 @@
 <a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
 </p>
 
-## About Laravel
+## About this project
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
+Company Management System is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. This takes the pain out of development by easing common tasks used in many web projects, such as:
 
 - [Simple, fast routing engine](https://laravel.com/docs/routing).
 - [Powerful dependency injection container](https://laravel.com/docs/container).
@@ -18,6 +18,8 @@ Laravel is a web application framework with expressive, elegant syntax. We belie
 - Database agnostic [schema migrations](https://laravel.com/docs/migrations).
 - [Robust background job processing](https://laravel.com/docs/queues).
 - [Real-time event broadcasting](https://laravel.com/docs/broadcasting).
+
+<img src="dashboard.png" width="1200">
 
 Laravel is accessible, powerful, and provides tools required for large, robust applications.
 
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,25 +40,29 @@
                     <h4 class="text-muted text-center font-size-18"><b>Sign In</b></h4>
 
                     <div class="p-3">
-                        <form class="form-horizontal mt-3" action="index.html">
+                        <form class="form-horizontal mt-3" method="POST" action="{{ route('login') }}">
+                            @csrf
 
                             <div class="form-group mb-3 row">
                                 <div class="col-12">
-                                    <input class="form-control" type="text" required="" placeholder="Username">
+                                    <input class="form-control" type="email" name="email" placeholder="Email"
+                                        required>
                                 </div>
                             </div>
 
                             <div class="form-group mb-3 row">
                                 <div class="col-12">
-                                    <input class="form-control" type="password" required="" placeholder="Password">
+                                    <input class="form-control" type="password" name="password" placeholder="Password"
+                                        required>
                                 </div>
                             </div>
 
                             <div class="form-group mb-3 row">
                                 <div class="col-12">
                                     <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="form-label ms-1" for="customCheck1">Remember me</label>
+                                        <input type="checkbox" class="custom-control-input" name="remember"
+                                            id="remember_me">
+                                        <label class="form-label ms-1" for="remember_me">Remember me</label>
                                     </div>
                                 </div>
                             </div>
@@ -72,11 +76,12 @@
 
                             <div class="form-group mb-0 row mt-2">
                                 <div class="col-sm-7 mt-3">
-                                    <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock"></i> Forgot
+                                    <a href="{{ route('password.request') }}" class="text-muted"><i
+                                            class="mdi mdi-lock"></i> Forgot
                                         your password?</a>
                                 </div>
                                 <div class="col-sm-5 mt-3">
-                                    <a href="auth-register.html" class="text-muted"><i
+                                    <a href="{{ route('register') }}" class="text-muted"><i
                                             class="mdi mdi-account-circle"></i> Create an account</a>
                                 </div>
                             </div>
