aabdbf16025fa8cf93eb50200cc62655ce3b990f
sohan <sohanmd381@gmail.com>
2023-03-01 11:47:23 +0600

    login image

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,10 +31,8 @@
                     <div class="text-center mt-4">
                         <div class="mb-3">
                             <a href="index.html" class="auth-logo">
-                                <img src="{{ asset('backend/assets/images/logo-dark.png') }}" height="30"
-                                    class="logo-dark mx-auto" alt="">
-                                <img src="{{ asset('backend/assets/images/logo-light.png') }}" height="30"
-                                    class="logo-light mx-auto" alt="">
+                                <img src="{{ asset('logo.jpg') }}" height="30" class="logo-dark mx-auto"
+                                    alt="">
                             </a>
                         </div>
                     </div>
